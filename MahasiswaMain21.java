5a0e2f4c8f202e23d88609cf0dd7a2f65d4ae2a9
Nerazuriancta <nerasyahputri@gmail.com>
2026-02-19 19:22:31 +0700

    Percobaan 3

--- MahasiswaMain21.java
+++ MahasiswaMain21.java
@@ -1,14 +1,18 @@
 public class MahasiswaMain21 {
     public static void main(String[] args) {
         Mahasiswa21 mhs1 = new Mahasiswa21();
-        mhs1.nama = "Nera";
+        mhs1.nama = "Nerazuriancta";
         mhs1.nim = "254107020117";   
         mhs1.kelas = "TI-1D";
         mhs1.ipk = 4.00;
 
         mhs1.tampilkanInformasi();
         mhs1.ubahKelas("TI-1B");
-        mhs1.updateIpk(3.70);
+        mhs1.updateIpk(3.90);
         mhs1.tampilkanInformasi();
+
+        Mahasiswa21 mhs2 = new Mahasiswa21("Ayunda", "254107020118",3.90, "TI-1D");
+        mhs2.updateIpk(4.00);
+        mhs2.tampilkanInformasi();
     }
 }
